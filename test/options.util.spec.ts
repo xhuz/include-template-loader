94e91fb0a84695b767b67f00aec1e084ebe3a140
huzz <83443122@qq.com>
2020-05-26 18:15:30 +0800

    fix: coverage not working

--- test/options.util.spec.ts
+++ test/options.util.spec.ts
@@ -1,8 +1,37 @@
 import {validateOptions, mergeOptions} from '../src/utils/options-util';
+import * as loaderUtils from 'loader-utils';
+import {Options} from '../src/interface';
+
+jest.mock('loader-utils');
+
+const options: Options = {
+  sign: ['<%=', '%>'],
+  deep: 1
+};
 
 describe('template parser util', () => {
+  const getOptionsSpy = jest.spyOn(loaderUtils, 'getOptions');
+  beforeEach(() => {
+    getOptionsSpy.mockClear();
+  });
   it('to be defined', () => {
     expect(validateOptions).toBeDefined();
     expect(mergeOptions).toBeDefined();
+  });
+
+  describe('validateOptions', () => {
+    it('should return a options', () => {
+      const spy = getOptionsSpy.mockReturnValue(options);
+      expect(validateOptions({} as any)).toEqual(options);
+      expect(spy).toBeCalled();
+    });
+  });
+
+  describe('mergeOptions', () => {
+    it('should merge options', () => {
+      const obj1: Options = {sign: ['<', '>'], deep: 4};
+      const obj2: Partial<Options> = {deep: 10};
+      expect(mergeOptions(obj1, obj2)).toEqual({sign: ['<', '>'], deep: 10});
+    });
   });
 });
--- test/template-parser.util.spec.ts
+++ test/template-parser.util.spec.ts
@@ -1,7 +1,45 @@
 import {templateParser} from '../src/utils/template-parser.util';
+import {Options} from '../src/interface';
+jest.mock('loader-utils');
+
+const source = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    {{@include('../components/header.html', {"name": "123"})}}
+
+    <img src="../assets/images/1.png" alt="" />
+    <nav>
+      <a href="page-a/index.html">page-a</a>
+      <a href="page-a/sub-page-a/index.html">page-a/sub-page-a</a>
+      <a href="page-a/sub-page-a/sub-sub-page-a/index.html">
+        page-a/sub-page-a/sub-sub-page-a
+      </a>
+      <a href="page-b/index.html">page-b</a>
+      <a href="page-c/index.html">page-c</a>
+    </nav>
+
+    {{@include('../components/footer.html', {"name": 123})}}
+  </body>
+</html>`;
+
+const options: Options = {
+  sign: ['{{', '}}'],
+  deep: 3
+};
 
 describe('template parser util', () => {
   it('to be defined', () => {
     expect(templateParser).toBeDefined();
+  });
+
+  describe('templateParser', () => {
+    it('should return [string, Set<string>]', () => {
+      templateParser({} as any, source, options);
+    });
   });
 });
